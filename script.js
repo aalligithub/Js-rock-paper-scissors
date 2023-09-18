5896e61a83982b5ee2b302ea939b3b52120c0020
Ali <83744482+aalligithub@users.noreply.github.com>
2023-09-19 01:14:36 +0330

    comments

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+// putting our choices in an array so we can use it to decide the computer's choice
 const choices = ['Rock', 'Paper', 'Scissors'];
 
+// variables used to store the occourance of each outcome
 let wins = 0;
 let loses = 0;
 let draws = 0;
 
+// functions that change the UI
 function draw(){
     document.getElementById("conclusion").innerHTML = "Draw!";
     draws += 1;
@@ -22,6 +25,7 @@ function loss(){
     document.getElementById("loses").textContent = "Loses : " + loses;
 }
 
+// here we use random to generate a number between 0 to 2, 0 meaning the first in choices array (Rock) and so forth
 function rock(){
     const random = Math.floor(Math.random() * choices.length);
     document.getElementById("computer-output").innerHTML = "Computer : " + choices[random];
